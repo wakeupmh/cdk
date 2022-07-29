74fc667bd07ade839501bf8d5c4611b9fe8455cd
wakeupmh <wakeupmh@gmail.com>
2022-07-29 17:59:35 -0300

    feat: :bricks: add queue and dlq

--- sqs-cdk/lib/sqs/simple-queue-service.ts
+++ sqs-cdk/lib/sqs/simple-queue-service.ts
@@ -1,10 +1,28 @@
+import { CfnOutput } from "aws-cdk-lib";
+import { Queue } from "aws-cdk-lib/aws-sqs";
 import { Construct } from "constructs";
 
 export default class SimpleQueueService extends Construct {
-    constructor(scope: Construct, id: string, props: any) {
+  constructor(scope: Construct, id: string, props: any) {
+    super(scope, id);
 
-        super(scope, id);
+    const deadLetterQueue = new Queue(this, "dlq", {
+      queueName: `my-${props.env}-dlq`,
+    });
 
-        // here you can put all your sqs resources based on the use case
-    }
+    const queueName = `my-${props.env}-queue`
+    const queue = new Queue(this, 'analytics-event-processor', {
+      queueName,
+      deadLetterQueue: {
+        queue: deadLetterQueue,
+        maxReceiveCount: 3, //The number of times a message can be unsuccessfully dequeued 
+      },
+    })
+    //these following lines are just to make the output and you can use in another projects
+    new CfnOutput(this, 'application-integration', {
+      value: queue.queueArn,
+      exportName: `application-integration::${queueName}::arn`,
+    })
+    
+  }
 }
